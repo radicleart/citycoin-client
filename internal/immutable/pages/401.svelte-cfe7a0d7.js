0549650bec577636ebc23ba5f5ba5fc3672ebc6a
radicleart <mjoecohen@gmail.com>
2023-01-13 15:58:05 +0000

    Updates

---
+++ internal/immutable/pages/401.svelte-cfe7a0d7.js
@@ -0,0 +1,5 @@
+import{S as i,i as u,s as d,l,r as m,N as h,m as p,h as s,u as _,p as c,K as f,b as x,E as o}from"../chunks/index-81fcc74e.js";function y(r){let t,a;return{c(){t=l("meta"),a=m(`
+
+Not Found`),this.h()},l(e){const n=h('[data-svelte="svelte-1ds1qyu"]',document.head);t=p(n,"META",{name:!0,content:!0}),n.forEach(s),a=_(e,`
+
+Not Found`),this.h()},h(){document.title="About",c(t,"name","description"),c(t,"content","About this app")},m(e,n){f(document.head,t),x(e,a,n)},p:o,i:o,o,d(e){s(t),e&&s(a)}}}function b(r){return[]}class v extends i{constructor(t){super(),u(this,t,b,y,d,{})}}export{v as default};
